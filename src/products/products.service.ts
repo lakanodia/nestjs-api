4c642fb8abf96b6db45768967394ba92b34e2b9e
Laka Nodia <laka.nodia@gmail.com>
2023-10-10 16:14:52 +0400

    Create update method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -16,10 +16,31 @@ export class ProductsService {
   }
 
   getSingleProduct(productId: string) {
-    const product = this.products.find((prod) => prod.id === productId);
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.desc = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
     if (!product) {
       throw new NotFoundException('Could not found product!');
     }
-    return { ...product };
+    return [product, productIndex];
   }
 }
